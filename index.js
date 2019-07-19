c121b9d79a82343019a251b180cdeb1dc8a71166
Matthew C <m23.cullen@gmail.com>
2019-07-19 16:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function takeANumber(currentLine,newName){
   var position = currentLine.length;
   return `Welcome, ${newName}. You are number ${position} in line.`;
 }
+
+function nowServing(currentLine){
+  if (currentLine === []){
+    return "There is no one in line.";
+  } else {
+    var serving = currentLine.
+  }
+}
