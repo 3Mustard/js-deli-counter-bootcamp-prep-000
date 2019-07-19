68a36cfa5b7eccbbf323bd8a6053baa795545512
Matthew C <m23.cullen@gmail.com>
2019-07-19 16:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function takeANumber(currentLine,newName){
   var line = [];
   line.push(currentLine);
   line.push(newName);
-  
+  var position = line[-1];
+  return `Welcome, ${newName}. You are number ${position} in line.`;
 }
