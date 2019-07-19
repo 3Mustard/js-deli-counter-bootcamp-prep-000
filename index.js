fb0575f856f1a4c7fddede005c0bc16ac1df465f
Matthew C <m23.cullen@gmail.com>
2019-07-19 16:39:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ function nowServing(currentLine){
     return `Currently serving ${serving}.`;
   }
 }
+
+function currentLine(current){
+  if (current.length === 0){
+    return "The line is currently empty";
+  } else {
+    for(var i = 0; i < current.length; i++){
+      
+    }
+  }
+}
