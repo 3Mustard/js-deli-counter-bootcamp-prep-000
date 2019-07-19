453a215b65c51f5ebc2673dbc2be00ec02c446ba
Matthew C <m23.cullen@gmail.com>
2019-07-19 16:43:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(current){
   if (current.length === 0){
     return "The line is currently empty";
   } else {
-    for(var i = 0; i < current.length; i++){
+    for (var i = 1; i <= current.length; i++){
       
     }
   }
